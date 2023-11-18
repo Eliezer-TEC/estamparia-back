40f9f96e7b73852914ee41647eafe6360e9766e4
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-11-17 22:35:47 -0300

    17/11- Desenv. as entidades

--- src/main/java/estampariaback/estamparia/dw/model/entity/Endereco.java
+++ src/main/java/estampariaback/estamparia/dw/model/entity/Endereco.java
@@ -1,5 +1,102 @@
 package estampariaback.estamparia.dw.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "endereco")
+@NoArgsConstructor
 public class Endereco {
+	private int id;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String numero;
+	private String complemento;
+
+	public Endereco() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Endereco(int id, String rua, String bairro, String cidade, String estado, String cep, String numero,
+			String complemento) {
+		super();
+		this.id = id;
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+		this.numero = numero;
+		this.complemento = complemento;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
 
 }
--- src/main/java/estampariaback/estamparia/dw/model/entity/Pedido.java
+++ src/main/java/estampariaback/estamparia/dw/model/entity/Pedido.java
@@ -1,5 +1,14 @@
 package estampariaback.estamparia.dw.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pedido")
+@NoArgsConstructor
 public class Pedido {
+	private int id;
+	
 
 }
